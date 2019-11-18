62923983361fc7bbf508dab3bae9bc400fa7db84
Aleksandr Barmin <barmin.alexander@gmail.com>
2019-11-18 13:10:52 +0300

    Ribbon finally works

--- cloud-eureka/eureka-client-declarative/src/main/java/com/epam/dsc2019/cloud/ribbon/client/GreetingServiceRibbonImpl.java
+++ cloud-eureka/eureka-client-declarative/src/main/java/com/epam/dsc2019/cloud/ribbon/client/GreetingServiceRibbonImpl.java
@@ -1,26 +1,17 @@
 package com.epam.dsc2019.cloud.ribbon.client;
 
 import com.epam.dsc2019.cloud.discovery.client.GreetingService;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@RibbonClient(
-        name = "cloud-eureka-client",
-        configuration = RibbonConfiguration.class
-)
 public class GreetingServiceRibbonImpl implements GreetingService {
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Override
-    public String getGreeting() throws Exception {
-        return this.restTemplate().getForObject("http://cloud-eureka-client/greeting", String.class);
+    public String getGreeting() {
+        return restTemplate.getForObject("http://cloud-eureka-client/greeting", String.class);
     }
 }
--- cloud-eureka/eureka-client-declarative/src/main/java/com/epam/dsc2019/cloud/ribbon/client/RibbonConfiguration.java
+++ cloud-eureka/eureka-client-declarative/src/main/java/com/epam/dsc2019/cloud/ribbon/client/RibbonConfiguration.java
@@ -1,24 +1,28 @@
 package com.epam.dsc2019.cloud.ribbon.client;
 
-import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.PingUrl;
 import com.netflix.loadbalancer.WeightedResponseTimeRule;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@RibbonClients({
+    @RibbonClient(
+        name = "cloud-eureka-client",
+        configuration = RibbonConfiguration.class
+    )
+})
 public class RibbonConfiguration {
-    @Autowired
-    IClientConfig ribbonClientConfig;
-
     @Bean
-    public IPing ribbonPing(IClientConfig config) {
-        return new PingUrl();
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     @Bean
-    public IRule ribbonRule(IClientConfig config) {
+    public IRule eurekaClientSelectionRule() {
         return new WeightedResponseTimeRule();
     }
 }
