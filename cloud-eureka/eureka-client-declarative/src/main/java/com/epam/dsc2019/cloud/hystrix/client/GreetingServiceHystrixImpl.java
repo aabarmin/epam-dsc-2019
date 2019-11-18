7dc958dc10b11f6cfd82be08e86427adb730901c
Aleksandr Barmin <barmin.alexander@gmail.com>
2019-11-18 13:16:06 +0300

    Hystrix works

--- cloud-eureka/eureka-client-declarative/src/main/java/com/epam/dsc2019/cloud/hystrix/client/GreetingServiceHystrixImpl.java
+++ cloud-eureka/eureka-client-declarative/src/main/java/com/epam/dsc2019/cloud/hystrix/client/GreetingServiceHystrixImpl.java
@@ -5,31 +5,31 @@ import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class GreetingServiceHystrixImpl implements GreetingService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GreetingServiceHystrixImpl.class);
-
     @Autowired
     private RestTemplate restTemplate;
     @Autowired
     private EurekaClient eurekaClient;
+
     @Value("${app.external.service.name}")
     private String serviceName;
 
     @HystrixCommand(fallbackMethod = "getDefaultGreeting")
     @Override
-    public String getGreeting() throws Exception {
+    public String getGreeting() {
         final Application application = eurekaClient.getApplication(serviceName);
         final InstanceInfo instance = application.getInstances().get(0);
         final String url = "http://" + instance.getHostName() + ":" + instance.getPort() + "/greeting";
-        LOGGER.info("Ask URL {} for greeting", url);
+
+        log.info("Ask URL {} for greeting", url);
 
         return restTemplate.getForObject(url, String.class);
     }
